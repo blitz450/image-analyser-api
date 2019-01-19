018a52b784e22291fca083cdc287e3f7694922e0
blitz450 <saurabh.chauhan450@gmail.com>
2019-01-20 00:50:36 +0530

    added database

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'localhost:5432',
-    user : 'LOL',
-    password : '4386',
-    database : 'image'
+    connectionString: process.env.DATABASE_URL,
+  	 ssl: true,
   }
 });
 
