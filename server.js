1c6e8611cb6e86054b0e046303fc0a4d713983db
blitz450 <saurabh.chauhan450@gmail.com>
2019-01-20 01:20:47 +0530

    added database 3

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
+    database : 'postgresql-reticulated-13436',
     connectionString: process.env.DATABASE_URL,
   	 ssl: true,
-  	 database: 'postgresql-reticulated-13436'
   }
 });
 
